5366e0aeb6fe013a67c652778292a15226f7307e
boris <boris.musatov@gmail.com>
2014-03-08 16:54:40 +0100

    - back button

--- app/src/main/java/cz/boris/demo/NetworkActivity.java
+++ app/src/main/java/cz/boris/demo/NetworkActivity.java
@@ -2,6 +2,7 @@ package cz.boris.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import cz.boris.demo.fragment.NetworkFragment;
 
@@ -15,6 +16,18 @@ public class NetworkActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_container);
         getFragmentManager().beginTransaction().add(R.id.fragment_container, NetworkFragment.newInstance()).commit();
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
